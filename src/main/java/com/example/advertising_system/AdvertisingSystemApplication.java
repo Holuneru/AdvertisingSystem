bb2b0a0d1ed9ed2baea14498027cb69fd1e576ab
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-02 15:16:11 +0300

    Start

---
+++ src/main/java/com/example/advertising_system/AdvertisingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.advertising_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingSystemApplication.class, args);
+	}
+
+}
