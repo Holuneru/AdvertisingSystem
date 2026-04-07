629ca99467550f176d235fed6d7321ccaa19d112
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-07 18:36:47 +0300

    FULL Fix bug about NoClickable Announcement Second

--- src/main/java/com/example/advertising_system/Service/AdvertiserService/ClickedClientService.java
+++ src/main/java/com/example/advertising_system/Service/AdvertiserService/ClickedClientService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+
 public class ClickedClientService {
     private final ClickedClientRepo clickedClientRepo;
     
