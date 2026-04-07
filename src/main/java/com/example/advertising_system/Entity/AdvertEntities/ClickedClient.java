e396db2b50526487e06e4d8da3c3c34b66af96bd
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-04-07 13:02:56 +0000

    FULL Fix bug about NoClickable Announcement DONE

---
+++ src/main/java/com/example/advertising_system/Entity/AdvertEntities/ClickedClient.java
@@ -0,0 +1,28 @@
+package com.example.advertising_system.Entity.AdvertEntities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "clicked_clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ClickedClient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String client_id;
+    private Long announcement_id;
+
+
+
+}
